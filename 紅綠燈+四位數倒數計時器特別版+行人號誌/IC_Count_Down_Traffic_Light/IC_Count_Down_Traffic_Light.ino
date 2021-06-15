ae6223991101ce17e50c52f09144a86a4027ae17
r0930514 <36184972+r0930514@users.noreply.github.com>
2021-06-15 10:03:49 +0800

    完成四位數七段顯示器特別版與行人號誌整合

--- 紅綠燈+四位數倒數計時器特別版+行人號誌/IC_Count_Down_Traffic_Light/IC_Count_Down_Traffic_Light.ino
+++ 紅綠燈+四位數倒數計時器特別版+行人號誌/IC_Count_Down_Traffic_Light/IC_Count_Down_Traffic_Light.ino
@@ -13,27 +13,6 @@ int YELLOW_LED_PIN =  4;  // 設定LED_YELLOW的接腳為4號腳位
 int buttonState = 0;
 Led4digit74HC595 myLedDisplay(A2, A1, A0);	// 接腳:(SCLK, RCLK, DIO)
 
-
-//函式
-//輸入一個數字決定7段顯示器顯示1-9
-//NM為該函式所輸入值，在二維陣列的第幾排（數字幾）
-void Seven_Write(byte NM)
-{
-    int time =1500; //設定要幾毫秒
-	int twodisplaydelay =5; //設定兩個顯示器之間的延遲
-    while (1)
-    {
-		myLedDisplay.loopShow();	// 每一次loop都要呼叫這個函式
-  		myLedDisplay.setNumber(NM);  //顯示數字內容
-		delay(twodisplaydelay);
-		time = time - 2*twodisplaydelay ;        //時間計算
-        if (time <0)
-        {
-            break;
-        }
-    }
-}
-
 //小綠人動畫
 byte dot_Led_control [9][8] = {
 {
@@ -74,6 +53,9 @@ byte dot_Led_control [9][8] = {
   B00000010
 },{
   B11000000,
+  B11000000,
+  B01100000,
+  B01111000,
   B10110100,
   B00111010,
   B00100110,
@@ -116,6 +98,37 @@ byte dot_Led_control [9][8] = {
   B01100110
 }
 };
+
+//函式
+//輸入一個數字決定7段顯示器顯示1-9
+//NM為該函式所輸入值，在二維陣列的第幾排（數字幾）
+void Seven_Write(byte NM)
+{
+  int time = 0;
+  int animation = 0;
+    while (1)
+    {
+		myLedDisplay.loopShow();	// 每一次loop都要呼叫這個函式
+  	myLedDisplay.setNumber(NM);  //顯示數字內容
+		delay(5);
+    time = time +5;
+    if (time % 125 == 0)
+    {
+      animation = time / 125 -1;
+      for(int row=0; row<8; row++){
+          dot_Led.setRow(0, row, dot_Led_control[animation][row]);
+      }
+    }
+    
+    if (time >1000)
+    {
+      break;
+    }
+    
+    }
+}
+
+
 void setup() {
   	dot_Led.shutdown(0, false);
     dot_Led.setIntensity(0, 2);
@@ -155,32 +168,11 @@ void loop() {
         digitalWrite(YELLOW_LED_PIN, 0);       //LED_YELLOW關閉
         digitalWrite(RED_LED_PIN, HIGH);       //LED_RED開啟
         delay(2000);
-        for(int j=0; j<7; j++)
-        {
-            for(int i=0; i<8; i++)
-            {
-                for(int row=0; row<8; row++)
-                {
-                    dot_Led.setRow(0, row, dot_Led_control[i][row]);
-                }
-                delay(100);
-            }
-        }
-        for(int j=0; j<10; j++)
-        {
-            for(int i=0; i<8; i++)
-            {
-                for(int row=0; row<8; row++)
-                {
-                    dot_Led.setRow(0, row, dot_Led_control[i][row]);
-                }
-                delay(60);
-            }
+        for(int i=10; i>0; i--){
+            Seven_Write(i);
         }
         for(int row=0; row<8; row++){
-            {
-                dot_Led.setRow(0, row, dot_Led_control[8][row]);
-            }
+          dot_Led.setRow(0, row, dot_Led_control[8][row]);
         }     
         delay(1000);               
         digitalWrite(RED_LED_PIN, 0);          //LED_RED關閉
